83c9f29653a66e3fdd0cabba59453e078e98378a
samarthsoni3002 <samarthsoni3002@gmail.com>
2024-04-21 10:32:42 +0530

    Model Loaded and backend integrated withf frontend

--- app.py
+++ app.py
@@ -6,15 +6,39 @@ from nltk.corpus import stopwords
 import numpy as np
 import nltk
 from review_scraper import scrape_amazon_reviews
-from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Sequential,Model
 from tensorflow.keras.layers import Lambda
 import tensorflow_hub as hub
 import tensorflow as tf
 from tensorflow.keras.layers import Embedding,Dense,LSTM,Dropout,SpatialDropout1D,TextVectorization,GRU
 
 
+embedding = "https://tfhub.dev/google/nnlm-en-dim50/2"
+hub_layer = hub.KerasLayer(embedding, input_shape=[],
+                           dtype=tf.string, trainable=True)
 
-model = load_model("./review_model.keras")
+class MyModel(Model):
+    def __init__(self):
+        super(MyModel, self).__init__()
+        self.embedding_layer = hub_layer
+        self.reshape_layer = tf.keras.layers.Reshape((1,-1))
+        self.lstm_layer1 = tf.keras.layers.LSTM(100,dropout=0.2,input_dim=(50), return_sequences=True,activation="relu")
+        self.lstm_layer2 = tf.keras.layers.LSTM(100, dropout=0.2,return_sequences=True,activation="relu")
+        self.gru_layer3 = tf.keras.layers.GRU(100,activation="relu")
+        self.output_layer = tf.keras.layers.Dense(5, activation="softmax")
+        
+    def call(self, inputs):
+        x = self.embedding_layer(inputs)
+        x = self.reshape_layer(x)
+        x = self.lstm_layer1(x)
+        x = self.lstm_layer2(x)
+        x = self.gru_layer3(x)
+        return self.output_layer(x)
+    
+
+model = MyModel()
+model.load_weights("./review_model.keras")
+
 
 
 app = Flask(__name__)
@@ -54,10 +78,9 @@ def predict():
     
     final_predictions = int(summ/len(reviews))
 
+    
+    return render_template("index.html", prediction_text="The predicted score is {}".format(final_predictions))
 
-    #return render_template("index.html", prediction_text="The predicted score is {}".format(np.argmax(prediction)))
-    #print(final_predictions)
-    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
